f7dc1b90a26cc939e6b9b1763499d9f65ad412da
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2014-01-17 23:37:29 +0100

    Implemented list export in csv/json/bbCode.
    closes #15

---
+++ js/directives/export_link.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.module('jlogApp.directives')
+    .directive('exportLink', function() {
+        return {
+            restrict: 'A',
+            transclude: true,
+            scope: {
+                name: '@',
+                type: '@',
+                source: '='
+            },
+            template: '\
+  <a> \
+  </a> \
+  <a class="clickable" \
+     ng-click="exportSource()" \
+     ng-transclude> \
+  </a> \
+',
+            controller: ['$scope', function($scope) {
+                $scope.file_url = null;
+                $scope.file_name = $scope.name + '.' + $scope.type;
+
+                $scope.exportSource = function() {
+                    var data = $scope.source.export($scope.type);
+
+                    console.log('generate ' + $scope.type + ' file');
+                    var old_url = $scope.file_url;
+                    $scope.file_url = null;
+                    if(old_url !== null) {
+                        URL.revokeObjectURL(old_url);
+                    }
+                    var blob = new Blob([data], {type: 'text/plain'});
+                    window.URL = window.URL || window.webkitURL;
+                    var url = window.URL.createObjectURL(blob);
+                    $scope.file_url = url;
+
+                    var today = new Date();
+                    $scope.file_name = $scope.name
+                        + '_' + today.getTime()
+                        + '.' + $scope.type;
+
+                    $scope.launchDownload();
+                };
+
+            }],
+            link: function(scope, iElement, iAttrs) {
+                var download_link = iElement.find('a')[0];
+                scope.launchDownload = function() {
+                    download_link.href = scope.file_url;
+                    download_link.download = scope.file_name;
+                    download_link.click();
+                };
+            }
+        }
+    });
--- js/services/battleListDisplay.js
+++ js/services/battleListDisplay.js
@@ -22,7 +22,225 @@ angular.module('jlogApp.services')
                                                     sort_types[sort.type].key,
                                                     sort.reverse);
             };
-            return {
+            function exportCsvKeys(battle, keys) {
+                var key;
+                var result = '';
+                for(key in battle) {
+                    if( battle.hasOwnProperty(key) &&
+                        '$' !== key[0] ) {
+                        switch(key) {
+                        case 'my_army':
+                            {
+                                result += 'my_army_faction' + ',';
+                                result += 'my_army_caster' + ',';
+                                break;
+                            }
+                        case 'opponent':
+                            {
+                                result += 'opponent_name' + ',';
+                                result += 'opponent_faction' + ',';
+                                result += 'opponent_caster' + ',';
+                                break;
+                            }
+                        case 'points':
+                            {
+                                result += 'my_scenario_points' + ',';
+                                result += 'my_army_points' + ',';
+                                result += 'opponent_scenario_points' + ','; 
+                                result += 'opponent_army_points' + ',';
+                                break;
+                            }
+                        case 'setup':
+                            {
+                                result += 'size' + ',';
+                                result += 'scenario' + ',';
+                                result += 'event' + ','; 
+                                break;
+                            }
+                        case 'initiative':
+                            {
+                                result += 'init_dice' + ',';
+                                result += 'init_start' + ',';
+                                break;
+                            }
+                        default:
+                            {
+                                result += key + ',';
+                                break;
+                            }
+                        }
+                        keys.push(key);
+                    }
+                }
+                result = result.slice(0, result.length-1);
+                result += '\r\n';
+                return result;
+            };
+            function exportBbKeys(battle, keys) {
+                var key;
+                var result = '[tr]';
+                for(key in battle) {
+                    if( battle.hasOwnProperty(key) &&
+                        '$' !== key[0] ) {
+                        switch(key) {
+                        case 'my_army':
+                            {
+                                result += '[th]' + 'my_army_faction' + '[/th]';
+                                result += '[th]' + 'my_army_caster' + '[/th]';
+                                break;
+                            }
+                        case 'opponent':
+                            {
+                                result += '[th]' + 'opponent_name' + '[/th]';
+                                result += '[th]' + 'opponent_faction' + '[/th]';
+                                result += '[th]' + 'opponent_caster' + '[/th]';
+                                break;
+                            }
+                        case 'points':
+                            {
+                                result += '[th]' + 'my_scenario_points' + '[/th]';
+                                result += '[th]' + 'my_army_points' + '[/th]';
+                                result += '[th]' + 'opponent_scenario_points' + '[/th]'; 
+                                result += '[th]' + 'opponent_army_points' + '[/th]';
+                                break;
+                            }
+                        case 'setup':
+                            {
+                                result += '[th]' + 'size' + '[/th]';
+                                result += '[th]' + 'scenario' + '[/th]';
+                                result += '[th]' + 'event' + '[/th]'; 
+                                break;
+                            }
+                        case 'initiative':
+                            {
+                                result += '[th]' + 'init_dice' + '[/th]';
+                                result += '[th]' + 'init_start' + '[/th]';
+                                break;
+                            }
+                        default:
+                            {
+                                result += '[th]' + key + '[/th]';
+                                break;
+                            }
+                        }
+                        keys.push(key);
+                    }
+                }
+                result += '[/tr]\r\n';
+                return result;
+            };
+            function exportCsvValues(battle, keys) {
+                var key, j, result='';
+                for(j= 0 ; j < keys.length ; j++) {
+                    key = keys[j];
+                    var entry = battle[key];
+                    switch(key) {
+                    case 'date':
+                        {
+                            result += entry.year + '/'
+                                + entry.month + '/'
+                                + entry.day + ',';
+                            break;
+                        }
+                    case 'opponent':
+                        {
+                            result += entry.name + ',';
+                            // FALLTHROUGH !!!
+                        }
+                    case 'my_army':
+                        {
+                            result += entry.faction + ',';
+                            result += entry.caster + ',';
+                            break;
+                        }
+                    case 'points':
+                        {
+                            result += entry.my_army.scenario + ',';
+                            result += entry.my_army.army + ',';
+                            result += entry.opponent.scenario + ',';
+                            result += entry.opponent.army + ',';
+                            break;
+                        }
+                    case 'setup':
+                        {
+                            result += entry.size + ',';
+                            result += entry.scenario + ',';
+                            result += entry.event + ',';
+                            break;
+                        }
+                    case 'initiative':
+                        {
+                            result += entry.dice + ',';
+                            result += entry.start + ',';
+                            break;
+                        }
+                    default:
+                        {
+                            result += entry + ',';
+                            break;
+                        }
+                    }
+                }
+                result = result.slice(0, result.length-1);
+                result += '\r\n';
+                return result;
+            };
+            function exportBbValues(battle, keys) {
+                var key, j, result='[tr]';
+                for(j= 0 ; j < keys.length ; j++) {
+                    key = keys[j];
+                    var entry = battle[key];
+                    switch(key) {
+                    case 'date':
+                        {
+                            result += '[td]' + entry.year + '/'
+                                + entry.month + '/'
+                                + entry.day + '[/td]';
+                            break;
+                        }
+                    case 'opponent':
+                        {
+                            result += '[td]' + entry.name + '[/td]';
+                            // FALLTHROUGH !!!
+                        }
+                    case 'my_army':
+                        {
+                            result += '[td]' + entry.faction + '[/td]';
+                            result += '[td]' + entry.caster + '[/td]';
+                            break;
+                        }
+                    case 'points':
+                        {
+                            result += '[td]' + entry.my_army.scenario + '[/td]';
+                            result += '[td]' + entry.my_army.army + '[/td]';
+                            result += '[td]' + entry.opponent.scenario + '[/td]';
+                            result += '[td]' + entry.opponent.army + '[/td]';
+                            break;
+                        }
+                    case 'setup':
+                        {
+                            result += '[td]' + entry.size + '[/td]';
+                            result += '[td]' + entry.scenario + '[/td]';
+                            result += '[td]' + entry.event + '[/td]';
+                            break;
+                        }
+                    case 'initiative':
+                        {
+                            result += '[td]' + entry.dice + '[/td]';
+                            result += '[td]' + entry.start + '[/td]';
+                            break;
+                        }
+                    default:
+                        {
+                            result += '[td]' + entry + '[/td]';
+                            break;
+                        }
+                    }
+                }
+                result += '[/tr]\r\n';
+                return result;
+            };
+            var instance= {
                 battles: [],
                 sorted_battles: [],
                 slice: 15,
@@ -41,6 +259,48 @@ angular.module('jlogApp.services')
                     if(this.more) {
                         more.call(this);
                     }
+                },
+                export: function displayBattleExport(format) {
+                    var result = '';
+                    if(0 >= instance.sorted_battles.length) return result;
+                    var list = instance.sorted_battles;
+                    switch(format) {
+                    case 'csv':
+                        {
+                            var keys = [];
+                            result += exportCsvKeys(list[0], keys);
+                            var i;
+                            for(i = 0 ; i<list.length ; i++) {
+                                result += exportCsvValues(list[i], keys);
+                            }
+                            break;
+                        }
+                    case 'bb':
+                        {
+                            var keys = [];
+                            result += '[table]\r\n';
+                            result += exportBbKeys(list[0], keys);
+                            var i;
+                            for(i = 0 ; i<list.length ; i++) {
+                                result += exportBbValues(list[i], keys);
+                            }
+                            result += '[/table]\r\n';
+                            break;
+                        }
+                    case 'json':
+                        {
+                            result += JSON.stringify(list);
+                            break;
+                        }
+                    default:
+                        {
+                            console.log('displayBattleListExport unknown format ' + format);
+                            break;
+                        }
+                    };
+                    console.log(result);
+                    return result;
                 }
-            }
+            };
+            return instance;
         }]);
