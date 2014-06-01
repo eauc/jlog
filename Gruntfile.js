fc17d4c4f46e8518607cdd2c52b14b21cba2ff6c
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2014-06-02 00:22:28 +0200

    Added jshint:test to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,33 @@ module.exports = function(grunt) {
         files: {
           src: [ "client/js/**/*.js" ]
         }
+      },
+      tests: {
+        options: {
+          multistr: true,
+          globals: {
+            "angular": false,
+            "$": false,
+            "console": false,
+            "describe": false,
+            "it": false,
+            "beforeEach": false,
+            "expect": false,
+            "module": false,
+            "inject": false,
+            "jasmine": false,
+          },
+          "-W097": true, // "use strict function form"
+          "-W040": true // "possible strict violation"
+        },
+        files: {
+          src: [ "spec/javascripts/**/*.js" ]
+        }
       }
     },
     watch: {
       scripts: {
-        files: "client/js/**/*.js",
+        files: [ "client/js/**/*.js", "spec/javascripts/**/*.js" ],
         tasks: [ "jshint:scripts" ],
         options: {
           spawn: true
